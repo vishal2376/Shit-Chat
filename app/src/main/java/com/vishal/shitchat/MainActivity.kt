4e51855efdefec51a99b59f7d2c373f9bf677e89
vishal2376 <vishalsingh2376@gmail.com>
2022-09-14 14:14:12 +0530

    testing data added

--- app/src/main/java/com/vishal/shitchat/MainActivity.kt
+++ app/src/main/java/com/vishal/shitchat/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.vishal.shitchat
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.vishal.shitchat.adapters.ChatAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //set recycler view
+        setRecyclerMA()
+
+    }
+
+    private fun setRecyclerMA() {
+
+        //temp data for testing
+        val tempData: ArrayList<Person> = ArrayList()
+        tempData.add(Person("Manik", "Join the world's best online", "08:10 PM", ""))
+        tempData.add(Person("Anshal", "Learn about basic Data Structure", "10:20 PM", ""))
+        tempData.add(Person("Tarkwadi", "Build products, practice skills", "08:40 PM", ""))
+        tempData.add(Person("Bhatia", "and in-person hackathons", "01:20 AM", ""))
+        tempData.add(Person("Manik", "Showing 7304 hackathons", "03:13 PM", ""))
+        tempData.add(Person("Hareta", "just use pan and paper", "04:10 AM", ""))
+        tempData.add(Person("Anshal", "Learn about basic Data Structure", "10:20 PM", ""))
+        tempData.add(Person("Tarkwadi", "Build products, practice skills", "08:40 PM", ""))
+        tempData.add(Person("Bhatia", "and in-person hackathons", "01:20 AM", ""))
+        tempData.add(Person("Tarkwadi", "Build products, practice skills", "08:40 PM", ""))
+        tempData.add(Person("Bhatia", "and in-person hackathons", "01:20 AM", ""))
+        tempData.add(Person("Manik", "Showing 7304 hackathons", "03:13 PM", ""))
+        tempData.add(Person("Hareta", "just use pan and paper", "04:10 AM", ""))
+        tempData.add(Person("Anshal", "Learn about basic Data Structure", "10:20 PM", ""))
+
+
+        rvChatList.layoutManager = LinearLayoutManager(this)
+        rvChatList.adapter = ChatAdapter(this, tempData)
     }
 }
