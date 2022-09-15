8fdc9e36050b4c05e30bddb1c93c291aece24279
vishal2376 <vishalsingh2376@gmail.com>
2022-09-15 21:30:12 +0530

    splash screen done

--- app/src/main/java/com/vishal/shitchat/SplashActivity.kt
+++ app/src/main/java/com/vishal/shitchat/SplashActivity.kt
@@ -1,11 +1,23 @@
 package com.vishal.shitchat
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                val i = Intent(this, MainActivity::class.java)
+                startActivity(i)
+                finish()
+            }, 1000
+        )
+
     }
 }
