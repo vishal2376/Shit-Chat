bca2b8ee2980784dd937b0fd3403235a6f2cfb6d
vishal2376 <vishalsingh2376@gmail.com>
2022-09-15 21:38:51 +0530

    animation added in splash screen

--- app/src/main/java/com/vishal/shitchat/SplashActivity.kt
+++ app/src/main/java/com/vishal/shitchat/SplashActivity.kt
@@ -4,12 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        //animation text
+        val txtAnimation = AnimationUtils.loadAnimation(this, R.anim.fade)
+        tvAppName.startAnimation(txtAnimation)
+
+        //animation logo
+        val logoAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_up)
+        imgAppLogo.startAnimation(logoAnimation)
 
         Handler(Looper.getMainLooper()).postDelayed(
             {
