44c862c900e48e371277bbd2728832402869a268
vishal2376 <vishalsingh2376@gmail.com>
2022-09-16 18:00:10 +0530

    fix: calling wrong button

--- app/src/main/java/com/vishal/shitchat/SignUpActivity.kt
+++ app/src/main/java/com/vishal/shitchat/SignUpActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +12,7 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sign_up)
 
         //sign up button
-        tvSignUp.setOnClickListener {
+        tvLogin.setOnClickListener {
             val i = Intent(this, LoginActivity::class.java)
             startActivity(i)
             finish()
