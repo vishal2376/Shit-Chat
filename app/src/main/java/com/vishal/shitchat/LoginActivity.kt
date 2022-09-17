ed854da6ed16cc1f190d2ca8a9d19f96dad18786
vishal2376 <vishalsingh2376@gmail.com>
2022-09-17 18:17:29 +0530

    firebase auth added

--- app/src/main/java/com/vishal/shitchat/LoginActivity.kt
+++ app/src/main/java/com/vishal/shitchat/LoginActivity.kt
@@ -4,18 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth:FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        //initialize
+        auth = FirebaseAuth.getInstance()
+
         //login button
         btnLogin.setOnClickListener {
-            val i = Intent(this,MainActivity::class.java)
-            startActivity(i)
-            finish()
+            val email = etEmailLA.text.toString()
+            val password = etPasswordLA.text.toString()
+
+            //login with email and password
+            login(email,password)
+
         }
 
         //google login button
@@ -23,11 +33,15 @@ class LoginActivity : AppCompatActivity() {
             Toast.makeText(this,"No implemented",Toast.LENGTH_SHORT).show()
         }
 
-        //sign up button
-        tvSignUp.setOnClickListener {
+        //sign up page button
+        tvSignUpLA.setOnClickListener {
             val i = Intent(this,SignUpActivity::class.java)
             startActivity(i)
             finish()
         }
+    }
+
+    private fun login(email: String, password: String) {
+
     }
 }
--- app/src/main/java/com/vishal/shitchat/SignUpActivity.kt
+++ app/src/main/java/com/vishal/shitchat/SignUpActivity.kt
@@ -3,19 +3,40 @@ package com.vishal.shitchat
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_login.*
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
+
+        //initialize
+        auth = FirebaseAuth.getInstance()
+
         //sign up button
-        tvLogin.setOnClickListener {
+        btnSignUp.setOnClickListener {
+            val name = etNameSA.text.toString()
+            val email = etEmailSA.text.toString()
+            val password = etPasswordSA.text.toString()
+
+            //login with email and password
+            signUp(name, email, password)
+        }
+
+        //login page button
+        tvLoginSA.setOnClickListener {
             val i = Intent(this, LoginActivity::class.java)
             startActivity(i)
             finish()
         }
+    }
+
+    private fun signUp(name: String, email: String, password: String) {
+
     }
 }
