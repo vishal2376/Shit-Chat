7ac00ff75568f022fbbdea8d6166fc749bf4d5ea
vishal2376 <vishalsingh2376@gmail.com>
2022-09-17 21:04:17 +0530

    load user database implemented

--- app/src/main/java/com/vishal/shitchat/MainActivity.kt
+++ app/src/main/java/com/vishal/shitchat/MainActivity.kt
@@ -1,51 +1,98 @@
 package com.vishal.shitchat
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.animation.AnimationUtils
 import android.view.animation.LayoutAnimationController
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.getValue
 import com.vishal.shitchat.adapters.UserAdapter
 import com.vishal.shitchat.models.User
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var dbUserRef: DatabaseReference
+
+    lateinit var userList: ArrayList<User>
+    lateinit var userAdapter: UserAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //initialize
+        auth = FirebaseAuth.getInstance()
+        dbUserRef = FirebaseDatabase.getInstance().getReference("user")
+
+        userList = ArrayList()
+        userAdapter = UserAdapter(this, userList)
+
         //set recycler view
         setRecyclerMA()
 
+        //load database
+        loadDatabase()
+
+
+        //logout button
+        btnLogoutMA.setOnClickListener {
+            logout()
+        }
+
+    }
+
+    private fun loadDatabase() {
+        dbUserRef.addValueEventListener(object : ValueEventListener {
+
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                userList.clear()
+
+                for (postSnapshot in snapshot.children) {
+                    val currentUser = postSnapshot.getValue<User>()
+                    userList.add(currentUser!!)
+                }
+
+                //update recycler view data
+                userAdapter.notifyDataSetChanged()
+            }
+
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+    //logout user and move to login screen
+    private fun logout() {
+        auth.signOut()
+
+        val i = Intent(this, LoginActivity::class.java)
+        startActivity(i)
+        finish()
     }
 
     private fun setRecyclerMA() {
 
-        //temp data for testing
-        val tempData: ArrayList<User> = ArrayList()
-        tempData.add(User("123","Manik","", "Join the world's best online", "08:10 PM", ""))
-        tempData.add(User("123","Anshal","", "Learn about basic Data Structure", "10:20 PM", ""))
-        tempData.add(User("123","Unknown","", "Build products, practice skills", "08:40 PM", ""))
-        tempData.add(User("123","Bhatia","", "and in-person hackathons", "01:20 AM", ""))
-        tempData.add(User("123","Manik", "","Showing 7304 hackathons", "03:13 PM", ""))
-        tempData.add(User("123","Mani", "","just use pan and paper", "04:10 AM", ""))
-        tempData.add(User("123","Anshal", "","Learn about basic Data Structure", "10:20 PM", ""))
-        tempData.add(User("123","Unknown","" ,"Build products, practice skills", "08:40 PM", ""))
-        tempData.add(User("123","Bhatia", "","and in-person hackathons", "01:20 AM", ""))
-        tempData.add(User("123","Unknown","" ,"Build products, practice skills", "08:40 PM", ""))
-        tempData.add(User("123","Bhatia","","and in-person hackathons", "01:20 AM", ""))
-        tempData.add(User("123","Manik", "","Showing 7304 hackathons", "03:13 PM", ""))
-        tempData.add(User("123","Mani", "","just use pan and paper", "04:10 AM", ""))
-        tempData.add(User("123","Anshal", "","Learn about basic Data Structure", "10:20 PM", ""))
-
-
+        //set layout and adapter
         rvChatList.layoutManager = LinearLayoutManager(this)
-        rvChatList.adapter = UserAdapter(this, tempData)
+        rvChatList.adapter = userAdapter
 
         //layout animation added
-        val chatListAnimation = LayoutAnimationController(AnimationUtils.loadAnimation(this,R.anim.slide_up))
-        chatListAnimation.delay = 0.06f
-        chatListAnimation.order = LayoutAnimationController.ORDER_NORMAL
-        rvChatList.layoutAnimation = chatListAnimation
+        val userListAnimation =
+            LayoutAnimationController(AnimationUtils.loadAnimation(this, R.anim.slide_up))
+        userListAnimation.delay = 0.06f
+        userListAnimation.order = LayoutAnimationController.ORDER_NORMAL
+        rvChatList.layoutAnimation = userListAnimation
     }
 }
--- app/src/main/java/com/vishal/shitchat/models/User.kt
+++ app/src/main/java/com/vishal/shitchat/models/User.kt
@@ -1,10 +1,10 @@
 package com.vishal.shitchat.models
 
 data class User(
-    val id:String,
-    val name:String,
-    val email:String,
-    val lastMessage:String,
-    val lastSeen:String,
-    val imgProfile:String
+    val id:String="",
+    val name:String="",
+    val email:String="",
+    val lastMessage:String="",
+    val lastSeen:String="",
+    val imgProfile:String=""
 )
